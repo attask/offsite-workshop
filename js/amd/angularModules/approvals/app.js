1faba4a142020712c84780f2a963c4a1806341b2
Matt Winchester <matthewwinchester@attask.com>
2014-01-20 18:10:51 -0700

    the answers

---
+++ js/amd/angularModules/approvals/app.js
@@ -0,0 +1,290 @@
+define([
+	'angular',
+	'angular-cookies',
+	'text!angularModules/approvals/templates/approvalProcess.html',
+	'text!angularModules/approvals/templates/approvalPath.html',
+	'text!angularModules/approvals/templates/approvalStep.html',
+
+	// Widgets
+	'angularModules/widgets/DropDown/DropDown',
+	'angularModules/widgets/TypeAhead/TypeAhead',
+
+	'angularModules/attask/i18n'
+], function(angular, ngCookies, approvalProcessTemplate, approvalPathTemplate, approvalStepTemplate){
+	var module = angular.module('approvals', ['ng', 'attask', 'attask.widgets', 'attask.i18n', 'attask.kamino', 'ngCookies']);
+
+	module.factory('statusEnum', ['StreamAPIService', 'ObjCodes', function(StreamAPIService, ObjCodes){
+		var statuses = {};
+
+		return function(objCode){
+			if(!statuses[objCode]){
+				switch(objCode){
+					case ObjCodes.Project:
+						statuses[objCode] = StreamAPIService.query({
+							objCode: ObjCodes.CustomEnum,
+							query: 'projectStatuses'
+						});
+
+						break;
+
+					case ObjCodes.Task:
+						statuses[objCode] =  StreamAPIService.query({
+							objCode: ObjCodes.CustomEnum,
+							query: 'taskStatuses'
+						});
+
+						break;
+
+					case ObjCodes.OpTask:
+						statuses[objCode] =  StreamAPIService.query({
+							objCode: ObjCodes.CustomEnum,
+							query: 'opTaskStatuses'
+						});
+
+						break;
+
+					default:
+						throw "ObjCode " + objCode + " doesn't support statuses";
+				}
+			}
+
+			return statuses[objCode];
+		};
+	}]);
+
+	module.service('ApprovalStepTypes', ['StreamAPIService', 'ObjCodes', 'getMessage', function(StreamAPIService, ObjCodes, getMessage){
+		return [
+			{
+				label: getMessage('approvaltype.rolebased'),
+				value: ObjCodes.Role
+
+			},
+			{
+				label: getMessage('approvaltype.teambased'),
+				value: ObjCodes.Team
+			},
+			{
+				label: getMessage('approvaltype.userbased'),
+				value: ObjCodes.User
+			}
+		];
+	}]);
+
+	module.directive('approvals', function(){
+		return {
+			scope: {
+				id: '@',
+				projectId: '@'
+			},
+			template: approvalProcessTemplate,
+			controllerAs: 'ctrl',
+			controller: ['$scope', 'getMessage', 'StreamAPIService', 'ObjCodes', '$http', '$cookies', '$timeout', function($scope, getMessage, StreamAPIService, ObjCodes, $http, $cookies, $timeout){
+				var that = this;
+
+				this.saveState = getMessage('action.save');
+
+				this.approvalProcess = $scope.id ? StreamAPIService.get({
+					objCode: ObjCodes.ApprovalProcess,
+					ID: $scope.id,
+					fields: [
+						'approvalPaths:*',
+						'approvalPaths:approvalSteps:*',
+						'approvalPaths:approvalSteps:stepApprovers:*',
+						'approvalPaths:approvalSteps:stepApprovers:user',
+						'approvalPaths:approvalSteps:stepApprovers:team',
+						'approvalPaths:approvalSteps:stepApprovers:role'
+					]
+				}) : new StreamAPIService({
+					objCode: ObjCodes.ApprovalProcess,
+					approvalPaths: [{
+						"rejectedStatus": "$$PREV",
+						"shouldCreateIssue": false,
+						approvalSteps:[{
+							stepApprovers: []
+						}]
+					}]
+				});
+
+				this.save = function(){
+					var aprvProc = angular.copy(this.approvalProcess),
+						proj = {
+						ID: $scope.projectId,
+						objCode: ObjCodes.Project,
+						assign: true,
+						approvalProcess: aprvProc
+					};
+
+					aprvProc = angular.extend(aprvProc, {
+						"assign":true,
+						"approvalObjCode":ObjCodes.Project,
+						"isPrivate":true,
+						"isCopy":false,
+						description: ''
+					});
+
+					aprvProc.approvalPaths.forEach(function(path){
+						path.assign = true;
+
+						path.approvalSteps.forEach(function(step){
+							step.assign = true;
+
+							step.stepApprovers.forEach(function(approver){
+								approver.assign = true;
+							});
+						});
+					});
+
+					this.saveState = getMessage('saving');
+
+					//EWWW SO UGRY!! WRY YOU USE SPRING CLIENT? BECUASE STREAM API DOESN'T LET US EDIT PRIVATE APPROVAL PROCESS... GAH!!!
+					$http.post('/approval/save', 'form=' + encodeURIComponent(JSON.stringify(proj)), {
+						headers: {
+							sessionID: ($cookies.attask || '').split('#')[0],
+							'Content-Type': 'application/x-www-form-urlencoded'
+						}
+					})
+						.then(function(){
+							that.saveState = getMessage('saved');
+
+							$timeout(function(){
+								that.saveState = getMessage('action.save');
+							}, 1000);
+						});
+				};
+			}]
+		};
+	});
+
+	module.directive('approvalPath', function(){
+		return {
+			scope: {
+				path: '='
+			},
+			template: approvalPathTemplate,
+			controllerAs: 'ctrl',
+			controller: ['statusEnum', 'ObjCodes', function(statusEnum, ObjCodes){
+				this.statuses = statusEnum(ObjCodes.Project);
+			}]
+		};
+	});
+
+	module.directive('approvalStep', function(){
+		return {
+			scope: {
+				step: '='
+			},
+			template: approvalStepTemplate,
+			controllerAs: 'ctrl',
+			controller: ['$scope', 'ApprovalStepTypes', 'ObjCodes', function($scope, ApprovalStepTypes, ObjCodes){
+				var originalApprovers = $scope.step.stepApprovers;
+
+				this.types = ApprovalStepTypes;
+
+				this.stepApprovers = {};
+				this.stepApprovers[ObjCodes.User] = [];
+				this.stepApprovers[ObjCodes.Team] = [];
+				this.stepApprovers[ObjCodes.Role] = [];
+
+				var that = this,
+					stepApprovers = $scope.step.stepApprovers;
+				for(var i = 0; i < stepApprovers.length; i++){
+					var detailObj = null;
+
+					if(stepApprovers[i].roleID){
+						detailObj = stepApprovers[i].role;
+					}
+					else if(stepApprovers[i].teamID){
+						detailObj = stepApprovers[i].team;
+					}
+					else if(stepApprovers[i].userID){
+						detailObj = stepApprovers[i].user;
+					}
+					else {
+						throw {
+							message: "Invalid step approver type",
+							obj: stepApprovers[i]
+						};
+					}
+
+					//This seems a little weird but it is the simplest way of figuring out what type of step this is
+					this.selectedType = detailObj.objCode;
+
+					this.stepApprovers[detailObj.objCode].push({
+						'value': detailObj.ID,
+						'label': detailObj.name,
+						'type': detailObj.objCode
+					});
+				}
+
+				this.stepApprovers[ObjCodes.Team] = this.stepApprovers[ObjCodes.Team][0];
+				this.stepApprovers[ObjCodes.Role] = this.stepApprovers[ObjCodes.Role][0];
+
+				function findApprover(approvers, approver){
+					if(!approvers || !approver){
+						return approver;
+					}
+
+					for(var i = 0; i < approvers.length; i++){
+						if(approvers[i].userID === approver.userID ){
+							return approvers[i];
+						}
+
+						if(approvers[i].teamID === approver.teamID){
+							return approvers[i];
+						}
+
+						if(approvers[i].roleID === approver.roleID){
+							return approvers[i];
+						}
+					}
+
+					return approver;
+				}
+
+				function updateApprovers(){
+					$scope.step.stepApprovers = [];
+
+					if(that.selectedType === ObjCodes.User){
+						for(var i = 0; i < that.stepApprovers[ObjCodes.User].length; i++){
+							$scope.step.stepApprovers.push(findApprover(originalApprovers, {
+								objCode: ObjCodes.StepApprover,
+								userID:that.stepApprovers[ObjCodes.User][i].value
+							}));
+						}
+
+						$scope.step.approvalType = 'ON';
+					}
+					else if(that.selectedType === ObjCodes.Team && that.stepApprovers[ObjCodes.Team]){
+						var item = findApprover(originalApprovers, {
+							objCode: ObjCodes.StepApprover,
+							teamID: that.stepApprovers[ObjCodes.Team].value
+						});
+
+						if(item){
+							$scope.step.stepApprovers.push(item);
+						}
+
+						$scope.step.approvalType = 'TB';
+					}
+					else if(that.selectedType === ObjCodes.Role && that.stepApprovers[ObjCodes.Role]){
+						var item = findApprover(originalApprovers, {
+							objCode: ObjCodes.StepApprover,
+							roleID: that.stepApprovers[ObjCodes.Role].value
+						});
+
+						if(item){
+							$scope.step.stepApprovers.push(item);
+						}
+
+						$scope.step.approvalType = 'RB';
+					}
+				}
+
+				$scope.$watchCollection('ctrl.stepApprovers', updateApprovers);
+				$scope.$watch('ctrl.selectedType', updateApprovers);
+			}]
+		};
+	});
+
+	return module;
+});
---
+++ specs/angularModules/approvals/appSpec.js
@@ -0,0 +1,260 @@
+define(['angular', 'angularModules/approvals/app'], function(angular){
+	describe('Approvals Setup', function(){
+		var $httpBackend, processElement, processScope,
+			baseProcess = getJSONFixture('approvals/approvalProcess.json'),
+			customStatuses = getJSONFixture('approvals/CSTEM.json'),
+			dropDown, typeAhead, oracle;
+
+		beforeEach(module('approvals'));
+
+		beforeEach(inject(function(_$httpBackend_, AtTaskWidgets){
+			var ARVPRC_REGEX = /\/attask\/api-internal\/ARVPRC\/1?[.]*/,
+				CSTEM_REGEX = /\/attask\/api-internal\/CSTEM\/1?[.]*/;
+
+			$httpBackend = _$httpBackend_;
+
+			$httpBackend.whenGET(ARVPRC_REGEX).respond(200, {data:baseProcess});
+			$httpBackend.whenGET(CSTEM_REGEX).respond(200, {data:customStatuses});
+
+			dropDown = jasmine.createSpyObj('dropDown', ['addEvent', 'clearItems', 'setValue', 'set', 'get', 'getPickList', 'setText']);
+			typeAhead = jasmine.createSpyObj('typeAhead', ['getOracle', 'addEvent', 'set', 'setValue', 'reset']);
+			oracle = jasmine.createSpyObj('oracle', ['set']);
+
+			spyOn(AtTaskWidgets, "create").andCallFake(function(widget){
+				if(widget === 'DropDown'){
+					return dropDown;
+				}
+				else if(widget === 'TypeAhead'){
+					return typeAhead;
+				}
+
+				return null;
+			});
+
+			dropDown.getPickList.andReturn(jasmine.createSpyObj('pickList', ['clearItems']));
+			typeAhead.getOracle.andReturn(oracle);
+		}));
+
+		beforeEach(inject(function($rootScope, $compile){
+			processElement = angular.element('<div approvals project-id="PROJ001" id="ARVPRC001"></div>');
+			processScope = $rootScope.$new();
+
+			processElement = $compile(processElement)(processScope);
+			processScope = processElement.isolateScope();
+
+			processScope.$digest();
+			$httpBackend.flush();
+
+			processScope.$digest();
+		}));
+
+		//flush the backend
+		afterEach(function () {
+			$httpBackend.verifyNoOutstandingExpectation();
+			$httpBackend.verifyNoOutstandingRequest();
+		});
+
+
+		it('should have data on the controller', function(){
+			expect(processScope.ctrl).toBeDefined();
+			expect(processScope.ctrl.approvalProcess).toBeDefined();
+			expect(processScope.ctrl.approvalProcess.name).toBe("Private Approval Process: My Project");
+			expect(processScope.ctrl.approvalProcess.approvalPaths).toBeDefined();
+			expect(processScope.ctrl.approvalProcess.approvalPaths.length).toBe(1);
+		});
+
+		it('should have a working save button', inject(function($timeout){
+			$httpBackend.expectPOST('/approval/save').respond(200);
+
+			var submitButton = processElement.find('button.primary');
+			expect(submitButton.text()).toBe('Save');
+
+			submitButton.click();
+			expect(submitButton.text()).toBe('Saving...');
+
+			$httpBackend.flush();
+			expect(submitButton.text()).toBe('Saved');
+
+			$timeout.flush();
+			expect(submitButton.text()).toBe('Save');
+		}));
+
+		describe('Approval Paths', function(){
+			var pathElement, pathScope, path;
+
+			beforeEach(function(){
+				pathElement = processElement.find('*[approval-path]');
+				expect(pathElement).toBeDefined();
+
+				pathScope = pathElement.isolateScope();
+				expect(pathScope).toBeDefined();
+
+				path = pathScope.path;
+				expect(path);
+			});
+
+			it('should have data', function(){
+				expect(path).toBeDefined();
+				expect(path).toMatch({
+					"ID": "ARVPTH001",
+					"objCode": "ARVPTH",
+					"approvalProcessID": "ARVPRC001",
+					"durationMinutes": 480,
+					"durationUnit": "D",
+					"rejectedStatus": "$$PREV",
+					"shouldCreateIssue": false,
+					"targetStatus": "CUR"
+				});
+
+				expect(path.approvalSteps).toBeDefined();
+				expect(path.approvalSteps.length).toBe(1);
+			});
+
+			it('should have targetStatus affect what shows', function(){
+				expect(pathElement.find('.steps-container').length).toBeTruthy();
+
+				path.targetStatus = null;
+				pathScope.$digest();
+
+				expect(pathElement.find('.steps-container').length).toBe(0);
+			});
+
+			describe('Approval Steps', function(){
+				var stepElement, stepScope, step;
+
+				beforeEach(function(){
+					stepElement = pathElement.find('*[approval-step]');
+					expect(stepElement).toBeDefined();
+
+					stepScope = stepElement.isolateScope();
+					expect(stepScope).toBeDefined();
+
+					step = stepScope.step;
+					expect(step).toBeDefined();
+				});
+
+				it('should have data', function(){
+					expect(step).toMatch({
+						"ID": "ARVSTP001",
+						"name": "One",
+						"objCode": "ARVSTP",
+						"approvalPathID": "ARVPTH001",
+						"approvalType": "RB",
+						"sequenceNumber": 0
+					});
+
+					expect(step.stepApprovers).toBeDefined();
+					expect(step.stepApprovers.length).toBe(1);
+
+					expect(step.stepApprovers[0]).toMatch({
+						"ID": "SPAPVR001", 
+						"objCode": "SPAPVR",
+						"approvalStepID": "ARVSTP001",
+						"roleID": "ROLE001",
+						"teamID": null,
+						"userID": null,
+						"wildCard": null,
+						"user": null,
+						"team": null,
+						"role": {
+							"ID": "ROLE001",
+							"name": "Engineer",
+							"objCode": "ROLE"
+						}
+					});
+				});
+
+				it('should show/hide typeaheads', function(){
+					expect(stepElement.find('*[oracle=ROLE]')).toBePresent();
+					expect(stepElement.find('*[oracle=USER]')).not.toBePresent();
+					expect(stepElement.find('*[oracle=TEAMOB]')).not.toBePresent();
+
+					stepScope.ctrl.selectedType = 'USER';
+					stepScope.$digest();
+
+					expect(stepElement.find('*[oracle=ROLE]')).not.toBePresent();
+					expect(stepElement.find('*[oracle=USER]')).toBePresent();
+					expect(stepElement.find('*[oracle=TEAMOB]')).not.toBePresent();
+
+					stepScope.ctrl.selectedType = 'TEAMOB';
+					stepScope.$digest();
+
+					expect(stepElement.find('*[oracle=ROLE]')).not.toBePresent();
+					expect(stepElement.find('*[oracle=USER]')).not.toBePresent();
+					expect(stepElement.find('*[oracle=TEAMOB]')).toBePresent();
+
+				});
+
+				it('should transform stepApprovers properly', function(){
+					expect(stepScope.ctrl.stepApprovers).toMatch({
+						USER: [],
+						TEAMOB: undefined,
+						ROLE: {
+							value: 'ROLE001',
+							label: 'Engineer',
+							type: 'ROLE'
+						}
+					});
+
+					expect(step.stepApprovers.length).toBe(1);
+					expect(step.stepApprovers).toContain({
+						"ID": "SPAPVR001",
+						"objCode": "SPAPVR",
+						"approvalStepID": "ARVSTP001",
+						"roleID": "ROLE001",
+						"teamID": null,
+						"userID": null,
+						"wildCard": null,
+						"user": null,
+						"team": null,
+						"role": {
+							"ID": "ROLE001",
+							"name": "Engineer",
+							"objCode": "ROLE"
+						}
+					});
+
+					stepScope.ctrl.selectedType = 'USER';
+					stepScope.$digest();
+
+					expect(step.stepApprovers.length).toBe(0);
+
+					stepScope.ctrl.stepApprovers = {
+						USER: [{
+							value: 'USER234'
+						}],
+						TEAMOB: undefined,
+						ROLE: undefined
+					};
+					stepScope.$digest();
+
+					expect(step.stepApprovers.length).toBe(1);
+					expect(step.stepApprovers).toContain({
+						objCode: 'SPAPVR',
+						userID: 'USER234'
+					});
+
+					stepScope.ctrl.selectedType = 'TEAMOB';
+					stepScope.$digest();
+
+					expect(step.stepApprovers.length).toBe(0);
+
+					stepScope.ctrl.stepApprovers = {
+						USER: [],
+						TEAMOB: {
+							value: 'TEAMOB2345'
+						},
+						ROLE: undefined
+					};
+					stepScope.$digest();
+
+					expect(step.stepApprovers.length).toBe(1);
+					expect(step.stepApprovers).toContain({
+						objCode: 'SPAPVR',
+						teamID: 'TEAMOB2345'
+					});
+				});
+			});
+		});
+	});
+});
